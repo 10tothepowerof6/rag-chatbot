d8c9f1abed7bc44739d2522ba85e88d82f767dfd
M1LL10N <xtrieu229@gmail.com>
2026-05-13 14:15:52 +0700

    feat: install pydantic library and test it to validate a class

--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from rag-chatbot!")
+from pydantic import BaseModel
 
+class UserQuery(BaseModel):
+    name: str
+    query: str
+    timestamp: float
+
+def main():
+    user = UserQuery(name="million", query="What's RAG?", timestamp="123456789")
+    
+    print(user.timestamp)
+    print(type(user.timestamp))
+    print(user.model_dump_json())
 
 if __name__ == "__main__":
     main()
