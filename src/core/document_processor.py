5f82567f691ec0b15a5f64151100765876277c41
M1LL10N <xtrieu229@gmail.com>
2026-05-31 22:54:09 +0700

    fix(core): add argument and change split logic in chunking logic
    - Add `data_path`  argument for `read_and_process_faq()`
    - Change split logic to keep the string "Question" in final `page_content`
    - Add docstring for `read_and_process_faq()`

--- src/core/document_processor.py
+++ src/core/document_processor.py
@@ -7,12 +7,29 @@ class Chunk(BaseModel):
     page_content: str
     metadata: dict
 
-def read_and_process_faq():
+def read_and_process_faq(data_path: Path = None):
+    """
+    Read all Markdown FAQ files in the data directory and split them into chunks.
+
+    This function scans for '*.md' files in the specified directory, extracts the topic 
+    (Category) from the first line of each file, and chunks the document based on each 
+    question formatted as "## Question: /question/ Content: /answer/".
+
+    Args:
+        data_path (Path, optional): The path to the directory containing the FAQ files. 
+                                    If not provided (or None), it defaults to the project's 
+                                    raw data path.
+
+    Returns:
+        list[Chunk]: A list of Chunk objects packed with metadata (source file and category).
+    """
+    if data_path is None:
+        data_path = raw_data_path
     chunks_list: list[Chunk] = []
-    for file_path in raw_data_path.glob('*.md'):
+    for file_path in data_path.glob('*.md'):
         # print(file_path);
         with open(file_path, "r", encoding="utf-8") as file:
-            raw_data = file.read().split("## Question: ")
+            raw_data = file.read().split("## ")
             current_category = raw_data[0].split('# Category: ')[-1].strip()
             current_metadata = {'source_file': file_path.name, 'category': current_category}
             for i in range(1, len(raw_data)):
