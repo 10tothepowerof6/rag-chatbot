5776fc8d88a775843d2d8c2c98923f7b29317d6d
M1LL10N <xtrieu229@gmail.com>
2026-06-06 23:47:53 +0700

    chore(deps): add ChromaDB and experiment with vector search in `main.py`
    - Add `chromadb` to project dependencies
    - Implement temporary spike code in `src/main.py` for indexing and querying

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
 from utils.llm import start_chat
 from core.document_processor import read_and_process_faq
+import chromadb
+chroma_cilent = chromadb.Client()
 
 if __name__ == "__main__":
-    # start_chat()
+    collection = chroma_cilent.get_or_create_collection(name="my_collection")
     clist = read_and_process_faq()
-    print(clist)
+    for chunk in clist:
+        collection.add(
+            ids=chunk.metadata['category'],
+            documents=chunk.page_content
+        )
+    query = input("Question: ")
+    results = collection.query(
+        query_texts=query,
+        n_results=2
+    )
+    print(results)
